117481ad2a1c6fdd1f6260a4318b4cf08f04e462
pankaj-kb <pankajb2601@gmail.com>
2023-01-18 22:44:47 +0530

    8th jan question 1

---
+++ 03 JavaScript/08 Jan 2023/Home Work/01 Question/variables.js
@@ -0,0 +1,17 @@
+// string, boolean, undefined and null data types, Display all the value with their data type.
+
+const myString = "This is an String";
+
+const myBoolean = true;
+
+const myUndefi = undefined;
+
+const myNull = null;
+
+console.log(myString + " of datatype " + typeof(myString));
+
+console.log(myBoolean + " of datatype " + typeof(myBoolean));
+
+console.log(myUndefi + " of datatype " + typeof(myUndefi));
+
+console.log(myNull + " of datatype " + typeof(myNull));
