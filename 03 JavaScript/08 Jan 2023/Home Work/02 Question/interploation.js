3b407340bbb079a382bf5700ccfdd0e569e2a94f
pankaj-kb <pankajb2601@gmail.com>
2023-01-18 22:53:30 +0530

    completed question two of 8th jan

---
+++ 03 JavaScript/08 Jan 2023/Home Work/02 Question/interploation.js
@@ -0,0 +1,13 @@
+// 02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+const firstName = "Pankaj";
+
+const lastName = "Bhadane";
+
+const maritalStatus = "Single";
+
+const country = "India";
+
+const age = 22;
+
+console.log(`First Name is ${firstName} Last name is ${lastName} marital Status is ${maritalStatus} nationality of ${country} and age is ${age}.`)
