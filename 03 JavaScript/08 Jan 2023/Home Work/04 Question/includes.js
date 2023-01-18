14b3d5a38a0329440c52510ffac890ab5c79d1d1
pankaj-kb <pankajb2601@gmail.com>
2023-01-18 23:06:07 +0530

    completed q4

---
+++ 03 JavaScript/08 Jan 2023/Home Work/04 Question/includes.js
@@ -0,0 +1,5 @@
+// 04. Declare a varibale and assign string value to it and check if the string contains a word Script using includes() method.
+
+let myString = "this is an very long string to check if it has the word Dragon"
+
+console.log(myString.includes("Dragon"))
