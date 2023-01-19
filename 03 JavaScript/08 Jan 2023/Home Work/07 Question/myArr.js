fa0ab84e25b380ea5e36952c2cbbf70ae1614900
pankaj-kb <pankajb2601@gmail.com>
2023-01-19 21:51:45 +0530

    question 7 complete

---
+++ 03 JavaScript/08 Jan 2023/Home Work/07 Question/myArr.js
@@ -0,0 +1,6 @@
+// 07. Declare an array containing the multiple values and use lastIndexOf to determine the position of the first and last occurrence of a word "pw skills".
+
+let myArr = ["my skils", "your skills", "pw skills", "thier skills", "pw skills", "all skills"];
+
+console.log(myArr.indexOf("pw skills"));
+console.log(myArr.lastIndexOf("pw skills"));
