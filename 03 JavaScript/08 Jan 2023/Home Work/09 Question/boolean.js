426aec2077bbdcbf968bb8bc6f33d8f763353f71
pankaj-kb <pankajb2601@gmail.com>
2023-01-19 22:03:40 +0530

    adding Question 9

---
+++ 03 JavaScript/08 Jan 2023/Home Work/09 Question/boolean.js
@@ -0,0 +1,14 @@
+// 09. Boolean value is either true or false.
+//     - Write three JavaScript statement example which provide truthy value.
+//     - Write three JavaScript statement example which provide falsy value.
+
+// Truthy
+console.log("50" == 50)
+console.log("pankaj".length == "orange".length)
+console.log(200 > 2)
+
+
+// Falsy
+console.log(1 > 2);
+console.log("pankaj".length == "pwskills".length)
+console.log("50" === 50)
