d9d11be44b671b72719de111b417833b347fd9f1
pankaj-kb <pankajb2601@gmail.com>
2023-01-25 22:52:53 +0530

    completed q15

--- 03 JavaScript/08 Jan 2023/Home Work/15 Question/marks.js
+++ 03 JavaScript/08 Jan 2023/Home Work/15 Question/marks.js
@@ -5,8 +5,22 @@
 //     - 50-59, D
 //     - 0-49, F
 
-let marks = 80;
+let marks = 90;
 
-switch(marks){
-    case(marks ==)
+switch (true) {
+    case (marks >= 80 && marks <= 100):
+        console.log("A")
+        break;
+    case (marks >= 70 && marks <= 89):
+        console.log("B")
+        break;
+    case (marks >= 60 && marks <= 69):
+        console.log("C");
+        break;
+    case (marks >= 50 && marks <= 59):
+        console.log("D");
+        break;
+    case (marks >= 0 && marks <= 49):
+        console.log("F");
+        break;
 }
