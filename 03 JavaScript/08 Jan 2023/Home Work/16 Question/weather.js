6098fb258c7603f4118fb89202178df2e4ec1533
pankaj-kb <pankajb2601@gmail.com>
2023-01-25 23:34:04 +0530

    completed q16

---
+++ 03 JavaScript/08 Jan 2023/Home Work/16 Question/weather.js
@@ -0,0 +1,31 @@
+// 16. Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+//     - September, October or November, the season is Autumn.
+//     - December, January or February, the season is Winter.
+//     - March, April or May, the season is Spring
+//     - June, July or August, the season is Summer
+
+
+let currentWeather = "March";
+
+switch (currentWeather) {
+    case "September":
+    case "October":
+    case "November":
+        console.log("Autumn")
+        break;
+    case "December":
+    case "January":
+    case "February":
+        console.log("Winter")
+        break;
+    case "March":
+    case "April":
+    case "May":
+        console.log("Spring")
+        break;
+    case "June":
+    case "July":
+    case "August":
+        console.log("Summer")
+        break;
+}
