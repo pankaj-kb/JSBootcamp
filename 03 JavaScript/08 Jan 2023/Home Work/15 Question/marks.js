455a36b721375e61d565cbbea85acc1c7ed12aac
pankaj-kb <pankajb2601@gmail.com>
2023-01-25 21:26:53 +0530

    15 question start

---
+++ 03 JavaScript/08 Jan 2023/Home Work/15 Question/marks.js
@@ -0,0 +1,12 @@
+// 15. Write a program which can give grades to students according to theirs scores:
+//     - 80-100, A
+//     - 70-89, B
+//     - 60-69, C
+//     - 50-59, D
+//     - 0-49, F
+
+let marks = 80;
+
+switch(marks){
+    case(marks ==)
+}
