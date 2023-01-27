1f86d72b2b964ea248f8c082fd19b4cafd156269
pankaj-kb <pankajb2601@gmail.com>
2023-01-27 20:36:42 +0530

    question 23 complete

---
+++ 03 JavaScript/08 Jan 2023/Home Work/23 Question/prime.js
@@ -0,0 +1,16 @@
+// 23. Write a program to check that the number given by the user is a prime number or not.
+
+
+function isPrime(n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPrime(5))
