824e3a62313bdad7a0b49f74b0363dc24ff93af9
pankaj-kb <pankajb2601@gmail.com>
2023-01-28 11:32:44 +0530

    completed q25

--- 03 JavaScript/08 Jan 2023/Home Work/25 Question/bmi.js
+++ 03 JavaScript/08 Jan 2023/Home Work/25 Question/bmi.js
@@ -6,3 +6,19 @@
 //     Normal weight: BMI is 18.5 to 24.9 -
 //     Overweight: BMI is 25 to 29.9 -
 //     Obese: BMI is 30 or more
+
+function bmiCheck(weight, height) {
+    let bmi = weight / (height * height);
+
+    if (bmi < 18.5) {
+        return Math.floor(bmi) + " Underweight";
+    } else if (bmi > 18.5 && bmi < 24.9) {
+        return Math.floor(bmi) + " Normal Weight";
+    } else if (bmi > 25 && bmi < 29.9) {
+        return Math.floor(bmi) + " Over-Weight"
+    } else if (bmi > 30) {
+        return Math.floor(bmi) + " Obese";
+    }
+}
+
+console.log(bmiCheck(60, 1.70))
