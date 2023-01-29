6fd90c1652b1040222309c2761d6517eb942db56
pankaj-kb <pankajb2601@gmail.com>
2023-01-29 17:07:03 +0530

    completed all

--- 03 JavaScript/08 Jan 2023/Home Work/30 Question/calculator.js
+++ 03 JavaScript/08 Jan 2023/Home Work/30 Question/calculator.js
@@ -1,2 +1,25 @@
 // 30. Create a simple calculator program in JavaScript which can perform the addition, substraction, multiplication and division on given numbers.
 
+function calculate(operation, number1, number2, ) {
+    if (operation === "add") {
+        return (number1 + number2);
+    }
+    if (operation === "substract") {
+        return (number1 - number2)
+    }
+    if (operation === "multiply") {
+        return (number1 * number2)
+    }
+    if (operation === "division") {
+        return (number1 / number2)
+    }
+    if (operation === "moduler") {
+        return (number1 % number2)
+    }
+}
+
+console.log(calculate("add", 5, 5));
+console.log(calculate("substract", 5, 5));
+console.log(calculate("multiply", 5, 5));
+console.log(calculate("division", 5, 5));
+console.log(calculate("moduler", 5, 5));
