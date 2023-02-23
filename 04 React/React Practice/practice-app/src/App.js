ebf62e5f703903ca02fad62cd1682cbc0aef97a3
pankaj-kb <pankajb2601@gmail.com>
2023-02-23 22:59:42 +0530

    adding react props practice

--- 04 React/React Practice/practice-app/src/App.js
+++ 04 React/React Practice/practice-app/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<div>
+  <Welcome name = "pankaj"/>
+    <Welcome name = "Gaurav"/>
+    <Welcome name = "Nishant"/>
+    <Welcome name = "Rohit"/>
+  </div>
+  
   );
 }
 
