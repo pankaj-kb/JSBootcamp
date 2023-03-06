62b6698d67c1ae1f84ece77d45c1ab2242be99b9
pankaj-kb <pankajb2601@gmail.com>
2023-03-06 23:06:47 +0530

    new files

---
+++ 04 React/counter-app/src/App.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import "./App.css";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [inLimit, setInLimit] = useState(0);
+  const [deLimit, setDeLimit] = useState(0);
+
+  const handleInLimit = (e) => {
+    setInLimit(e.target.value);
+  };
+
+  const handleDeLimit = (e) => {
+    setDeLimit(e.target.value);
+  };
+
+  const handleIncrease = (e) => {
+    setCount(count < inLimit ? count + 1 : count);
+  };
+
+  const handleDecrese = (e) => {
+    setCount(count > deLimit ? count - 1 : count)
+  };
+
+  return (
+    <div className="counter">
+      <h1>{count}</h1>
+      <button onClick={handleIncrease}>Increase Count</button>
+      <button onClick={handleDecrese}>Decrease Count</button>
+
+      <input
+        type="number"
+        placeholder="Enter Increase Limit"
+        value={inLimit}
+        onChange={handleInLimit}
+      />
+      <input
+        type="number"
+        placeholder="Enter Decrese Limit"
+        value={deLimit}
+        onChange={handleDeLimit}
+      />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
