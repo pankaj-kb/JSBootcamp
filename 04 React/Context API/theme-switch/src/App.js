990b3615e5655209d6b4937838f5dcabb1780518
pankaj-kb <pankajb2601@gmail.com>
2023-03-14 22:42:31 +0530

    fresh starrt

--- 04 React/Context API/theme-switch/src/App.js
+++ 04 React/Context API/theme-switch/src/App.js
@@ -1,15 +1,19 @@
+import "./App.css";
 import { ThemeProvider } from "./ThemeProvider";
 import { ThemeSwitcher } from "./ThemeSwitcher";
-import { ThemeContext } from "./context/ThemeContext";
+import { ThemeContext } from "./ThemeContext";
 import { useContext } from "react";
 
 export default function App() {
+  const { theme } = useContext(ThemeContext);
   return (
     <ThemeProvider>
-      <div className="App">
+      <div className={theme}>
         <h1>Let's Switch</h1>
         <ThemeSwitcher />
-        <button>Hello</button>
+        <p>
+          The Current Theme is <span>{theme}</span>
+        </p>
       </div>
     </ThemeProvider>
   );
--- 04 React/Context API/theme-switch/src/ThemeSwitcher.js
+++ 04 React/Context API/theme-switch/src/ThemeSwitcher.js
@@ -1,6 +1,6 @@
 // ThemeSwitcher.js
 import { useContext } from "react";
-import { ThemeContext } from "./context/ThemeContext";
+import { ThemeContext } from "./ThemeContext";
 import "./ThemeSwitcher.css";
 
 export const ThemeSwitcher = () => {
@@ -8,22 +8,24 @@ export const ThemeSwitcher = () => {
 
   const handleToggle = (event) => {
     toggleTheme();
+    // console.log(theme)
   };
 
   return (
-    <div className="toggle-container">
-      <input
-        type="checkbox"
-        id="toggle"
-        className="toggle-input"
-        checked={theme === "dark"}
-        onChange={handleToggle}
-      />
-      <label htmlFor="toggle" className="toggle-label">
-        <span className="toggle-text">
-          {theme === "light" ? "Light" : "Dark"} Mode
-        </span>
-      </label>
-    </div>
+    // <div className="toggle-container">
+    //   <input
+    //     type="checkbox"
+    //     id="toggle"
+    //     className="toggle-input"
+    //     checked={theme === "dark"}
+    //     onChange={handleToggle}
+    //   />
+    //   <label htmlFor="toggle" className="toggle-label">
+    //     <span className="toggle-text">
+    //       {theme === "light" ? "Light" : "Dark"} Mode
+    //     </span>
+    //   </label>
+    // </div>
+    <button onClick={handleToggle}>Switch ME</button>
   );
 };
